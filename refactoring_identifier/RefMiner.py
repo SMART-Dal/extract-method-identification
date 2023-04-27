7ce82c643cd25d3022b53d6d38ad44c265badafc
Indranil <indranil.palit@dal.ca>
2023-04-26 22:50:58 -0300

    refactor: changed environment variable dependency

--- refactoring_identifier/RefMiner.py
+++ refactoring_identifier/RefMiner.py
@@ -1,18 +1,24 @@
-import os, subprocess
+import os, subprocess,sys
+
 
 # print(os.environ['refresearch'])
 
 class RefMiner:
 
     def __init__(self) -> None:
-        self.ref_bin_path = os.path.join(os.environ['refresearch'],"refactoring_identifier","executable","RefactoringMiner","bin")
+        
         self.cwd = os.getcwd()
-        self.output_path = os.path.join(os.environ['refresearch'],"refactoring_identifier","output")
+        self.ref_bin_path = os.path.join(os.getcwd(),"refactoring_identifier","executable","RefactoringMiner","bin")
+        self.output_path = os.path.join(os.getcwd(),"refactoring_identifier","output")
     def exec_refactoring_miner(self, repo_path,repo_name):
         try:
             output_path = os.path.join(self.output_path,repo_name+".json")
             os.chdir(self.ref_bin_path)
-            ref_exec = subprocess.run(["sh","RefactoringMiner","-a",repo_path,"-json",output_path],capture_output=True, shell=True)
+            shell = True
+            if sys.platform == 'linux':
+                shell = False
+            
+            ref_exec = subprocess.run(["sh","RefactoringMiner","-a",repo_path,"-json",output_path],capture_output=True, shell=shell)
             ref_exec.check_returncode()
             os.chdir(self.cwd)
             return output_path
@@ -29,4 +35,5 @@ class RefMiner:
 
 
 if __name__=="__main__":
+    
     RefMiner().exec_refactoring_miner("/home/ip1102/projects/def-tusharma/ip1102/Ref-Res/Research/refactoring-toy-example","ref_toy")
