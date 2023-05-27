5cc85016be0c398b2f7f3317a3205a411d90edc4
Indranil <indranil.palit@dal.ca>
2023-05-27 10:43:26 -0700

    fix: updated output location

--- data_collector.py
+++ data_collector.py
@@ -64,31 +64,12 @@ def process_repo(repo_details):
     with lock:
 
 
-        # DB
-        # pg_db = PostgresDatabase()
-        # pg_db.cursor.execute("INSERT INTO repodetails (repo_name, repo_url) VALUES (%s, %s)", (db_dict["repo_name"], db_dict["repo_url"]))
-        # pg_db.connection.commit()
-        # repo_table_id = pg_db.cursor.fetchone()[0]
-
-        # for pos_method_code, neeg_method_code in zip(db_dict["positive_case_methods"], db_dict["negative_case_methods"]):
-        #     pg_db.cursor.execute("INSERT INTO sourcecodedata (repo_details_fkey, pos_source_code, neg_source_code) VALUES (%s, %s, %s)", (repo_table_id, pos_method_code, neeg_method_code))
-        #     pg_db.connection.commit()
-
-        # pg_db.connection.commit()
-        # pg_db.cursor.close()
-        # pg_db.connection.close()
-
-
         # Locally in jsonl 
-        out_jsonl_path = os.path.join(os.getcwd(),"data","output")
+        out_jsonl_path = os.path.join(os.environ['SLURM_TMPDIR'],'extract-method-identification',"data","output")
 
         if not os.path.exists(out_jsonl_path):
             os.mkdir(out_jsonl_path)
-        
-        # with open(os.path.join(out_jsonl_path,dotenv_values(".env")["output_file_name"]), 'a') as f: # os.environ["output_file_name"] doesn't work in MP
-        #     f.write(json.dumps(db_dict) + "\n")
-        #     f.flush()
-        #     f.close()
+
 
         with open(os.path.join(out_jsonl_path,output_file_name), 'a') as f: # os.environ["output_file_name"] doesn't work in MP
             f.write(json.dumps(db_dict) + "\n")
@@ -96,48 +77,7 @@ def process_repo(repo_details):
             f.close()
     
     print("End analysis for repo - "+repo_details[0])
-    # Generate Embeddings
-    # try:
-    #     gc_embedding_object = Bert("microsoft/graphcodebert-base")
-    #     pos_gc_embeddings = [gc_embedding_object.generate_individual_embedding(pos_method_body) for pos_method_body in pos_method_body_list]
-    #     neg_gc_embeddings = [gc_embedding_object.generate_individual_embedding(neg_method_body) for neg_method_body in neg_method_body_list]
 
-    #     db_dict = {
-    #         "repo_name":repo_details[0],
-    #         "repo_url": repo_details[1],
-    #         "positive_case_methods":pos_method_body_list,
-    #         "negative_case_methods":neg_method_body_list,
-    #         "positive_case_embedding": pos_gc_embeddings,
-    #         "negative_case_embedding":neg_gc_embeddings
-    #     }
-
-    #     gc_db.insert_doc(db_dict)
-    #     print(f"DB updated with GC for {repo_details[0]}")
-    # except Exception as e:
-    #     print(f"Error creating/updating graph code bert embeddings for repository - {repo_details[0]}")
-    #     return
-
-
-    # try:
-
-    #     cb_embedding_object = Bert("microsoft/codebert-base")
-    #     pos_cb_embeddings = [cb_embedding_object.generate_individual_embedding(pos_method_body) for pos_method_body in pos_method_body_list]
-    #     neg_cb_embeddings = [cb_embedding_object.generate_individual_embedding(neg_method_body) for neg_method_body in neg_method_body_list]
-
-    #     db_dict = {
-    #         "repo_name":repo_details[0],
-    #         "repo_url": repo_details[1],
-    #         "positive_case_methods":pos_method_body_list,
-    #         "negative_case_methods":neg_method_body_list,            
-    #         "positive_case_embedding": pos_cb_embeddings,
-    #         "negative_case_embedding":neg_cb_embeddings
-    #     }
-
-    #     cb_db.insert_doc(db_dict)
-    #     print(f"DB updated with CB for {repo_details[0]}")
-    # except Exception as e:
-    #     print(f"Error creating/updating code bert embeddings for repository - {repo_details[0]}")
-    #     return
 
 def run_process(NUM_WORKERS, process_repo):
     with open("/home/ip1102/Playground/multi_tp/data/input.csv", "r") as f:
