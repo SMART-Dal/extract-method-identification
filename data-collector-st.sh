35c7fc7ec1b6b7f93f1a5b8ce79a4fc16af2a428
Indranil <indranil.palit@dal.ca>
2023-05-29 12:20:17 -0700

    fix: modified the bash script to trap signals

--- data-collector-st.sh
+++ data-collector-st.sh
@@ -2,17 +2,22 @@
 #SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=3:0:0   
+#SBATCH --time=0:10:0   
 #SBATCH --mail-user=indranil.palit@dal.ca
 #SBATCH --mail-type=ALL
+#SBATCH --signal=B:USR1@180
 
+output_file_name=file_0000.jsonl
+echo "Start"
 
-function handle_signal {
-    echo 'Moving File'
-    rsync -avz --remove-source-files $SLURM_TMPDIR/extract-method-identification/data/output/test.jsonl $refresearch/data/output
+handle_signal() 
+{
+    echo 'Trapped - Moving File'
+    rsync -axvH --no-g --no-p $SLURM_TMPDIR/extract-method-identification/data/archive/$output_file_name $refresearch/data/output
+    exit 0
 }
 
-trap handle_signal SIGUSR1
+trap 'handle_signal' SIGUSR1
 
 
 cd $SLURM_TMPDIR
@@ -35,5 +40,17 @@ python -m venv res_venv
 source res_venv/bin/activate
 pip install -r requirements.txt
 
-python data_collector.py ./data/file_0000.csv file_0000.jsonl
+# -u is for unbuffered output so the print statements print it to the slurm out file
+# & at the end is to run the script in background. Unless it's running in background we can't trap the signal
+python -u data_collector.py ./data/file_0000.csv $output_file_name &  
+
+PID=$!
+wait ${PID}
+
+echo "Python Script execution over. Attempting to copy the output file..."
+
+rsync -axvH --no-g --no-p $SLURM_TMPDIR/extract-method-identification/data/archive/$output_file_name $refresearch/data/output
+
+echo "Completed data collection process."
+
 
